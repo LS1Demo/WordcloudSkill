b373231168a15edc8f2cb756b623cf95a7ddaf9d
Christof Steinkellner <christof.steinkellner@leftshift.one>
2020-12-16 11:33:09 +0000

    Initial wordcloud version

---
+++ src/handler.py
@@ -0,0 +1,57 @@
+import base64
+import logging
+
+from src.wordcloud.wordcloud import WordCloud
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("skill")
+
+
+def evaluate(payload: dict, context: dict) -> dict:
+    log.info(f"Received payload: {payload}")
+    log.info(f"Received context: {context}")
+
+    log.info("Wordcloud started!")
+
+    text = base64.b64decode(payload["text"].encode('utf-8'))
+
+    image = WordCloud.create_wordcloud(text)
+
+    log.info("Wordcloud finished!")
+
+    return {"image": base64.b64encode(image.encode("utf-8")).decode('utf-8')}
+
+
+def on_started(context: dict):
+    print("Wordcloud skill started!")
+
+
+def on_stopped(context: dict):
+    print("Wordcloud skill stopped!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
---
+++ src/wordcloud/wordcloud.py
@@ -0,0 +1,5 @@
+class WordCloud:
+
+    @staticmethod
+    def create_wordcloud(text: str) -> str:
+        return ""  # TODO
