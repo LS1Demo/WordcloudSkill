0535dbc3352c670b44ca8540b926a36389d6f905
Philipp Temmel <philipp.temmel@leftshift.one>
2020-12-21 09:53:55 +0100

    Initial wordcloud version

--- src/tests/wordcloud_test.py
+++ src/tests/wordcloud_test.py
@@ -1,19 +1,25 @@
 import base64
 import unittest
+from io import BytesIO
 
 from src.handler import evaluate
-from src.wordcloud.word_cloud_creator import WordCloudCreator
 
 
 class WordCloudBaseTest(unittest.TestCase):
 
-    # @staticmethod
-    # def test_wordcloud():
-    #     image = WordCloudCreator.create_wordcloud("My first wordcloud!")
-    #     print(image)
-
     @staticmethod
     def test_wordcloud():
+        with open('wordcloud_text_input', 'rb') as file:
+            text_bytes = BytesIO(file.read())
+            text_b64 = base64.b64encode(text_bytes.read()).decode('utf-8')
+
+        image = evaluate({"text": text_b64}, {})
+        print(image)
+
+    @staticmethod
+    @unittest.skip(reason="without file upload")
+    def test_wordcloud():
+
         text = "The first AIOS wordcloud!"
         text_b64 = base64.b64encode(bytes(text, 'utf-8')).decode('utf-8')
 
