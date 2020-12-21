7b8e50bcefb319c71a927f0dda7064619f83b9c9
Philipp Temmel <philipp.temmel@leftshift.one>
2020-12-21 09:53:55 +0100

    Added custom polarity color

--- src/wordcloud/word_cloud_creator.py
+++ src/wordcloud/word_cloud_creator.py
@@ -1,23 +1,40 @@
 import base64
 from io import BytesIO
+import random
 
-from os import path
-from PIL import Image
 import numpy as np
-import os
+from textblob import TextBlob
 
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import matplotlib.pyplot as plt
+
+from src.wordcloud.grouped_color_func import GroupedColorFunc
 
 
 class WordCloudCreator:
 
     @staticmethod
     def create_wordcloud(text: str, mask: np.ndarray = None) -> str:
-        wordcloud = WordCloud(background_color="white", max_words=100, mask=mask, max_font_size=40, width=1920, height=1080).generate(text)
+        text = "This word is repeated very very very very often but this other other other one as well house house house bad bad bad bad awsome awsome awsome awsome awsome " \
+               "charming charming charming!"
+        wordcloud = WordCloud(background_color="white", max_words=100, mask=mask, max_font_size=40, width=1920, height=1080, stopwords=STOPWORDS).generate(text)
+
+        green = '#00ff00'
+        red = '#ff0000'
+        color_to_words = {green: set([]), red: set([])}
+        tokens = text.split(sep=" ")
+        for token in tokens:
+            text_blob = TextBlob(token)
+            if text_blob.polarity > 0.0:
+                color_to_words[green].add(token)
+            elif text_blob.polarity < 0.0:
+                color_to_words[red].add(token)
+
+        image_colors = GroupedColorFunc(color_to_words, default_color='grey')
+        # image_colors = ImageColorGenerator(mask)  # use coloring of image mask
 
         plt.figure(figsize=[20, 20])
-        plt.imshow(wordcloud, interpolation="bilinear")
+        plt.imshow(wordcloud.recolor(color_func=image_colors), interpolation="bilinear")
         plt.axis("off")
 
         buf = BytesIO()
